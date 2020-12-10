864a1a883a4d25422e69b5b9d44e07b4625f2f91
Miriam Priscila <priscila@ENIGMA.com>
2020-12-09 23:10:20 -0600

    Burbujas

---
+++ ArregloBubbles/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArregloBubbles
+{
+    public partial class Form1 : Form
+    {
+        Ordenamiento O = new Ordenamiento();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            O.Agregar(n);
+            textBox1.Clear();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = O.Mostrar();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            O.Ordenar();
+            label2.Text = O.Mostrar();
+            MessageBox.Show(O.Ordenar());
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            O.VaciarLista();
+        }
+    }
+}
---
+++ ArregloBubbles/Ordenamiento.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArregloBubbles
+{
+    class Ordenamiento
+    {
+        int? aux;
+        int comparaciones = 0;
+        int intercambios = 0;
+        int index = 0;
+        int?[] lista = new int?[100];
+
+        public void Agregar(int n)
+        {
+            lista[index] = n;
+            index++;
+        }
+
+        public string Mostrar()
+        {
+            string numeros = "";
+            if (lista[0] != null)
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    numeros += lista[i] + ", ";
+                }
+                return numeros;
+            }
+            else
+            {
+                return "LA LISTA ESTÁ VACÍA.";
+            }
+
+        }
+
+        public string Ordenar()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 1; i < index; i++)
+            {
+                for (int j = 0; j < index - 1; j++)
+                {
+                    comparaciones++;
+                    if (lista[j] > lista[j + 1])
+                    {
+                        aux = lista[j];
+                        lista[j] = lista[j + 1];
+                        lista[j + 1] = aux;
+                        intercambios++;
+                    }
+                }
+            }
+            stopwatch.Stop();
+            string time = $"    Tiempo = {stopwatch.Elapsed.TotalMilliseconds } ms.";
+            Mostrar();
+            return "Comparaciones = " + comparaciones + "  Intercambios = " + intercambios + time;
+        }
+
+        public void VaciarLista()
+        {
+            Array.Clear(lista, 0, index);
+        }
+    }
+}
